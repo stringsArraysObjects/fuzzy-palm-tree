b9165ddcd29b917bec02d385afc6b4c9c3a2f560
Chris <chtuff@gmail.com>
2024-04-19 19:55:04 -0400

    solution

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ document.querySelector('button').addEventListener('click', getAnime)
 
  
 function getAnime() {
-    fetch('https://nekos.best/api/v2/neko',{cache: 'force-cache'})
+    fetch('https://nekos.best/api/v2/neko?amount=2',{cache: 'force-cache'})
   .then(response => response.json())
   .then(json => {
       console.log(json.results[0].url)
       document.querySelector('#anime').src = json.results[0].url
+      document.querySelector('#anime1').src = json.results[1].url
 
   })
 
