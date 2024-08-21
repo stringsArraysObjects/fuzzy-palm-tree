1b06889206016666a86fa57f0891009d083b64e6
Chris <chtuff@gmail.com>
2024-08-21 15:07:55 -0400

    now they can be easily downloaded

--- js/main.js
+++ js/main.js
@@ -14,11 +14,16 @@
       }
   
       const data = await response.json();
-      document.querySelector('#anime').src = data.results[0].url 
+      var animePicURL = data.results[0].url
+      var animePicURL1 = data.results[1].url
+      document.querySelector('#anime').src = animePicURL 
+      document.querySelector('#animeURL').href = animePicURL
       document.querySelector('#artistName').innerText = data.results[0].artist_name
-      document.querySelector('#anime1').src = data.results[1].url
+      document.querySelector('#anime1').src = animePicURL1
+      document.querySelector('#animeURL1').href = animePicURL1
       document.querySelector('#artistName1').innerText = data.results[1].artist_name
       console.log(data);
+      console.log(animePicURL)
     } catch (error) {
       console.error(error.message);
     }
@@ -26,17 +31,19 @@
 
 
 async function getGif() {
-  const url = 'https://nekos.best/api/v2/hug?amount=1';
+  const urlGif = 'https://nekos.best/api/v2/hug?amount=1';
   try {
-    const response = await fetch(url, {cache:"force-cache"});
+    const response = await fetch(urlGif, {cache:"force-cache"});
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const json = await response.json();
-      document.querySelector('#gif').src = json.results[0].url
-      document.querySelector('#artistName2').innerText = json.results[0].anime_name
-    console.log(json);
+    const data = await response.json();
+    var animeGifURL = data.results[0].url
+      document.querySelector('#gif').src = animeGifURL
+      document.querySelector('#animeGifURL').href = animeGifURL
+      document.querySelector('#artistName2').innerText = data.results[0].anime_name
+    console.log(data);
   } catch (error) {
     console.error(error.message);
   }
