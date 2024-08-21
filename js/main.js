39eee432a633c11d5811dd5983345a81030d4e9c
Mason <chtuff@gmail.com>
2024-08-21 14:19:22 -0400

    fixed layout to flexbox and removed buttons

--- js/main.js
+++ js/main.js
@@ -1,28 +1,43 @@
-document.querySelector('button').addEventListener('click', getAnime)
-const hideHeading = document.querySelector('div')
- 
-function getAnime() {
-    fetch('https://nekos.best/api/v2/neko?amount=2',{cache: 'force-cache'})
-  .then(response => response.json())
-  .then(json => {
-      console.log(json.results[0].url)
-      document.querySelector('#anime').src = json.results[0].url
-      document.querySelector('#anime1').src = json.results[1].url
-      hideHeading.classList.toggle('hidden')
-  })
 
-// https://nekos.best/api/v2/neko/XXXXXX-XXXXX.png
+ document.addEventListener("readystatechange", (event) => {
+  if(event.target.readyState === "interactive"){
+    getData()
+    getGif()
+  }
+ })
+  async function getData() {
+    const url = 'https://nekos.best/api/v2/neko?amount=2';
+    try {
+      const response = await fetch(url, {cache:"force-cache"});
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      document.querySelector('#anime').src = data.results[0].url 
+      document.querySelector('#artistName').innerText = data.results[0].artist_name
+      document.querySelector('#anime1').src = data.results[1].url
+      document.querySelector('#artistName1').innerText = data.results[1].artist_name
+      console.log(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 
-}
-document.querySelector('#gifButton').addEventListener('click', getGif)
 
-function getGif() {
-  fetch('https://nekos.best/api/v2/hug?amount=2',{cache: 'force-cache'})
-  .then(response => response.json())
-  .then(json => {
-      console.log(json.results[0].url)
+async function getGif() {
+  const url = 'https://nekos.best/api/v2/hug?amount=1';
+  try {
+    const response = await fetch(url, {cache:"force-cache"});
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
       document.querySelector('#gif').src = json.results[0].url
-
-  })
-
+      document.querySelector('#artistName2').innerText = json.results[0].anime_name
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
